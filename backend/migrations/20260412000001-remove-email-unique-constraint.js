f1ff9d7d360c66fc581e8fbd59a2371675a1855d
Dawit Yifter <dawityifter@gmail.com>
2026-04-12 22:51:03 -0500

    fix: use removeConstraint instead of removeIndex in email unique migration

--- backend/migrations/20260412000001-remove-email-unique-constraint.js
+++ backend/migrations/20260412000001-remove-email-unique-constraint.js
@@ -2,12 +2,13 @@
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.removeIndex('members', 'members_new_email_key');
+    await queryInterface.removeConstraint('members', 'members_new_email_key');
   },
 
   async down(queryInterface) {
-    await queryInterface.addIndex('members', ['email'], {
-      unique: true,
+    await queryInterface.addConstraint('members', {
+      fields: ['email'],
+      type: 'unique',
       name: 'members_new_email_key'
     });
   }
